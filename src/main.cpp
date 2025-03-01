17e7ea0bc81d11d9ff6c6704f559c10a9646e0d6
SenhorContritus <eduardo.martinez@unesp.br>
2025-03-01 09:21:56 -0300

    libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,68 @@
-#include <Arduino.h>
+#include<Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#define ftr 0
+#define cSens 2
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+int qnt = 0;
+
+LiquidCrystal_I2C lcd(0x27,20,4);
+
+void initLcd(){
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(6,0);
+  lcd.print("Gugu");
+  delay(1500);
+  lcd.setCursor(0,0);
+  lcd.clear();
+  lcd.print("Quantidade:");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+String verifyColor(int d){
+  delay(d);
+  if(cSens == 1){
+  	return  "Vermelho";
+  }
+  if(cSens == 2){
+  	return "Verde";
+  }
+  if(cSens == 3){
+    return "Azul";
+  }
+  else{
+  	return " undef.";
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void showData(int x, int d){
+  lcd.setCursor(0,0);
+  lcd.clear();
+  lcd.print("Quantidade:");
+  lcd.setCursor(11,0);
+  lcd.print(x);
+
+  lcd.setCursor(0,1);
+  lcd.print("Cor:");
+  lcd.setCursor(4,1);
+  lcd.print(verifyColor(d));
+}
+
+void setup()
+{
+  initLcd();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int ldr = analogRead(ftr);
+  if(ldr< 160){
+    qnt++;
+    showData(qnt, 200);
+    while(ldr < 160){
+      ldr = analogRead(ftr);
+    }
+  }	
+  
 }
